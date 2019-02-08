7233365b9e29f7ad7664a04d225cbb2558f37de3
Allen Hai <allenhai03@gmail.com>
2019-02-07 18:27:14 -0600

    init

---
+++ www/gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteMetadata: {
+    title: `Serverless Content Management`,
+    description: `mangage your portfolio with a serverless cms`,
+    author: `@coetry`
+  },
+  plugins: [
+    `gatsby-plugin-styled-jsx`,
+    `gatsby-plugin-react-helmet`,
+
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`
+  ]
+};
