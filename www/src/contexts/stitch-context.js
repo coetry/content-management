59348eecbd1b464013c47d52c4054c4e17ebca9d
Allen Hai <allenhai03@gmail.com>
2019-02-19 18:32:29 -0600

    add gatsby config to ignore mongodb-stitch-browser-sdk when building to avoid build-time errors

--- www/src/contexts/stitch-context.js
+++ www/src/contexts/stitch-context.js
@@ -7,23 +7,29 @@ const StitchProvider = ({ children }) => {
   const [state, setState] = useState(null);
 
   useEffect(() => {
-    const client = Stitch.initializeAppClient("content-management-mkiko");
+    import("mongodb-stitch-browser-sdk")
+      .then(sdk => {
+        const client = sdk.Stitch.initializeAppClient(
+          "content-management-mkiko"
+        );
 
-    const db = client
-      .getServiceClient(RemoteMongoClient.factory, "atlas")
-      .db("content");
+        const db = client
+          .getServiceClient(sdk.RemoteMongoClient.factory, "atlas")
+          .db("content");
 
-    const artifacts = db.collection("artifacts");
+        const artifacts = db.collection("artifacts");
 
-    console.log(db, artifacts);
+        console.log(db, artifacts);
 
-    setState({
-      client,
-      user: client.auth.user,
-      isLoggedIn: client.auth.isLoggedIn,
-      db,
-      artifacts
-    });
+        setState({
+          client,
+          user: client.auth.user,
+          isLoggedIn: client.auth.isLoggedIn,
+          db,
+          artifacts
+        });
+      })
+      .catch(console.error);
   }, []);
 
   return (
